cbae98bab86acc682f1661a9498ad8b1e1cba479
1gyeol-KIM <gksrufsla00@gmail.com>
2022-08-24 03:07:05 +0900

    modi

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.streamlit/
 
-sudo apt-get install curl
+sudo apt-get install curl git
 bash <(curl -s https://raw.githubusercontent.com/konlpy/konlpy/master/scripts/mecab.sh)
 
 echo "\
