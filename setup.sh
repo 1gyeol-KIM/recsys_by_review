2c1b4e8f40ab4f411e22071b7711f4eb833b5157
1gyeol-KIM <gksrufsla00@gmail.com>
2022-08-25 18:50:13 +0900

    modifed setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 mkdir -p ~/.streamlit/
 
-
 echo "\
 [server]\n\
 headless = true\n\
@@ -8,3 +7,7 @@ enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
 
+apt-get update
+apt-get install g++ openjdk-8-jdk 
+pip3 install konlpy JPype1-py3
+bash <(curl -s https://raw.githubusercontent.com/konlpy/konlpy/master/scripts/mecab.sh)
